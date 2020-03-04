52023557b4e002de61b32486d2c0da6c670c7242
Viktor Bergehall <viktorbergehall@gmail.com>
2020-03-04 17:36:32 +0100

    Ghpages

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
+  pathPrefix: '/hvitan',
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
